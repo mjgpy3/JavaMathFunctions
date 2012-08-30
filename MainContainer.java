79eb24bbf25afbed6648262bd78fa2122997d1fc
Michael Gilliland <mjg.py3@gmail.com>
2012-08-29 23:20:43 -0400

    Added some new functions and comments

--- MainContainer.java
+++ MainContainer.java
@@ -5,14 +5,24 @@ public class MainContainer
 
 	public static void main(String args[]) 
 	{
-		MichaelMath m = new MichaelMath();
-
-		System.out.println(m.pow(3, 3));
-		System.out.println(m.recPow(5, 3));
-		System.out.println(m.recNthFib(3));
-		System.out.println(m.recFactorial(4));
+		Integer[] arrayOfSequentialNumbers = {1,2,3,4,5,6,7,8,9,10,11};
 		
-		System.out.println(m.findModeOfArray(new String[] {"Michael", "Abbey", "Michael", "Five", "Abbey", "Michael", "Abbey", "Seven", "Abbey", "Car"}));
+		System.out.println(MichaelMath.pow(3, 3));
+		System.out.println(MichaelMath.recPow(5, 3));
+		System.out.println(MichaelMath.recNthFib(3));
+		System.out.println(MichaelMath.recFactorial(4));
+		
+		System.out.println(MichaelMath.findModeOfArray(new String[] {"Michael", "Abbey", "Michael", "Five", "Abbey", "Michael", "Abbey", "Seven", "Abbey", "Car"}));
+		System.out.println(MichaelMath.findModeOfArray(new Object[] {(int) 5, "Money", "Car", 5, "Money", "Money"}));
+		System.out.println(MichaelMath.isPrime(2) + ": 2");
+		System.out.println(MichaelMath.isPrime(3) + ": 3");
+		System.out.println(MichaelMath.isPrime(4) + ": 4");
+		System.out.println(MichaelMath.isPrime(234) + ": 234");
+		System.out.println(MichaelMath.isPrime(243) + ": 243");
+		System.out.println(MichaelMath.isPrime(97) + ": 97");
+		System.out.println(MichaelMath.abs(95) + " = |95|");
+		System.out.println(MichaelMath.abs(0) + " = |0|");
+		System.out.println(MichaelMath.abs(-95) + " = |-95|");
 		
 		int[] numbers = { 1, 2, 3, 4, 5, 34, 23, 34, 43, 23, 43213, 123 };
 
@@ -21,6 +31,10 @@ public class MainContainer
 			if (b > 10)
 				System.out.println(b);
 		}
+		
+		for (Object o : MichaelMath.cloneShuffle(arrayOfSequentialNumbers))
+		    System.out.print(o + ", ");
+		System.out.println();
 	}
 
 }
--- MichaelMath.java
+++ MichaelMath.java
@@ -1,10 +1,13 @@
 package com.michael.math;
 
 import java.util.HashMap;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class MichaelMath 
 {
-	public int Pow(int base, int exponent)
+	/// Returns some integer base to the power of an integer exponent
+	public static int pow(int base, int exponent)
 	{
 		int returnValue = 1;
 		for (int i = 0; i < exponent; i++)
@@ -12,31 +15,39 @@ public class MichaelMath
 		return returnValue;
 	}
 	
-	public int RecPow(int base, int exponent)
+	/// Returns some integer base to the power of an integer exponent, recursively
+	public static int recPow(int base, int exponent)
 	{
 		if (exponent == 1)
 			return base;
 		else
-			return base * RecPow(base, exponent - 1);
+			return base * recPow(base, exponent - 1);
 	}
 	
-	public int RecNthFib(int n)
+	/// Finds the Nth fibonacci number
+	public static int recNthFib(int n)
 	{
 		if (n <= 1)
 			return 1;
 		else
-			return RecNthFib(n - 2) + RecNthFib(n - 1);
+			return recNthFib(n - 2) + recNthFib(n - 1);
 	}
 	
-	public int RecFactorial(int x)
+	/// Performs the factorial operation recursively
+	public static int recFactorial(int x)
 	{
 		if (x <= 1)
 			return 1;
 		else
-			return x * RecFactorial(x - 1);
+			return x * recFactorial(x - 1);
 	}
 	
-	public Object FindModeOfArray(Object[] values)
+	/// Finds the most occurring object from a passed array of objects.
+	/// The array must have elements, and if there are multiple modes
+	/// then the first one is returned. e.g. findModeOfArray([1,2,1,2])
+	/// returns 1 since from the left to the right 1 is found as the mode
+	/// first.
+	public static Object findModeOfArray(Object[] values)
 	{
 		HashMap<Object, Integer> valueToOccur = new HashMap<Object, Integer>();
 		Object currentMode = new Object();
@@ -61,5 +72,40 @@ public class MichaelMath
 				currentMode = value;
 		}
 		return currentMode;
+	}
+	
+	/// Returns true if the passed number is a prime, false otherwise.
+	public static boolean isPrime(int number)
+	{
+		if (number <= 3)
+			return true;
+		if (number % 2 == 0)
+			return false;
+		for (int i = 3; i < Math.sqrt(number) + 1; i+=2)
+			if (number % i == 0)
+				return false;
+		return true;
+	}
+	
+	/// Returns the absolute value of a passed number.
+	public static int abs(int number)
+	{
+		return (number < 0) ? number * -1 : number;
+	}
+	
+	/// Returns a randomized version of the passed array.
+	public static Object[] cloneShuffle(Object[] array)
+	{
+		Object[] sorted = new Object[array.length];
+		Random generator = new Random();
+		ArrayList<Object> arrayList = new ArrayList<Object>();
+		
+		for (Object element : array)
+			arrayList.add(element);
+
+		for (int i = array.length - 1; i >= 0; i--)
+			sorted[i] = arrayList.remove(generator.nextInt(arrayList.size()));
+		
+		return sorted;
 	}
 }
